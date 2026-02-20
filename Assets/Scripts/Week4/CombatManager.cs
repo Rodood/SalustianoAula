b0a87f4202783e73dcb97d1ecc3e342edab5c068
Rodood <rodrigosqsfonseca@gmail.com>
2026-02-19 21:31:56 -0300

    Transição para batalha funcionando

--- Assets/Scripts/Week4/CombatManager.cs
+++ Assets/Scripts/Week4/CombatManager.cs
@@ -19,6 +19,9 @@ public class CombatManager : MonoBehaviour
         if(hero.GetComponent<PlayerMovement>() != null)
             hero.GetComponent<PlayerMovement>().enabled = false;
 
+        if(hero.GetComponent<InventoryInputs>() != null)
+            hero.GetComponent<InventoryInputs>().enabled = false;
+
         string enemy = GlobalData.enemyToGenerate;
         GameObject genereatedMonster = null;
 
